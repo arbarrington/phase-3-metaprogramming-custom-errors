d0727cc0c1a17bde3b3d0af7f64a5fea91a8d228
Austin Barrington <arbarrington12@gmail.com>
2022-09-21 21:00:28 -0600

    commit tree b5530c56733e48684ac46a6752acdfd4edca3069

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,14 +1,26 @@
 class Person
   attr_accessor :partner, :name
-
+  
+  
   def initialize(name)
     @name = name
+    PartnerError 
+  end
+
+  class PartnerError
+    def message
+      "error"
+    end
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else puts Person::PartnerError.new.message
+    end
   end
+  
 
 end
 
